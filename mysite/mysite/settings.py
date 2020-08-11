10a4acc5844fedb16a1bcdbf7d8dadd31d948eb9
kristianjackson <kristian.jackson@gmail.com>
2020-08-11 11:13:03 -0400

    added .env for Django secret

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -19,6 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+DJANGO_SECRET = os.getenv("DJANGO_SECRET")
 SECRET_KEY = 'l@t^0_d*2-_3^$6ni%4xy6$i0@9kxkrer0@^ht+6c4s#0!svp$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
